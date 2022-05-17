5e3100915e855d0c7da83c380ced988b0e57d9d8
lucaslurf <lucas_faria@outlook.com>
2022-05-17 12:21:43 -0300

    código adicionado

---
+++ helloWorldcomSpringboot/src/main/java/com/lurf/springboot/helloWorldcomSpringboot/HelloWorldcomSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.lurf.springboot.helloWorldcomSpringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldcomSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldcomSpringbootApplication.class, args);
+	}
+
+}
